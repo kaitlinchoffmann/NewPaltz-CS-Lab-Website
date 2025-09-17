aa1d2cc0433897cd45d742f2f39ae3a68894c580
LourisA <147088420+Loura1928@users.noreply.github.com>
2025-09-17 11:36:04 -0400

    merging main changes

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -66,10 +66,10 @@ router.post("/login", async (req, res) => {
             return res.status(500).json({ message: "Password hash not found" });
         }
 
-        // Compare provided password with stored hash
+        // // Compare provided password with stored hash
         const passwordMatch = await bcrypt.compare(password, userFound.password_hash);
 
-        // If password doesn't match
+        // // If password doesn't match
         if (!passwordMatch) {
             return res.status(401).json({ message: "Invalid credentials" });
         }
